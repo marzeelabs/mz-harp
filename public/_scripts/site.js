af5c57e3119f703fb4fc1970aa5757b4c7662e12
Jolidog <jolidog@gmail.com>
2016-08-01 10:20:40 +0100

    Reverted drupal day code.

--- public/_scripts/site.js
+++ public/_scripts/site.js
@@ -1,58 +1,29 @@
 $(function() {
+  var breakMobile = 730, // viewport px breakpoint
 
-  var breakMobile = 730; // viewport px breakpoint
+      fixedHeader = function() {
+        var viewportWidth = $( window ).width(),
+            fixedClass = 'navigation--fixed-top',
+            $navElement = $(".navigation");
 
-  fixedHeader = function() {
-    var viewportWidth = $( window ).width(),
-        fixedClass = 'navigation--fixed-top',
-        $navElement = $(".navigation");
+        if ($(window).scrollTop() > '1' && viewportWidth >= breakMobile) {
+          $navElement.addClass(fixedClass);
+        } else {
+          $navElement.removeClass(fixedClass);
+        }
+      };
 
-    if ($(window).scrollTop() > '1' && viewportWidth >= breakMobile) {
-      $navElement.addClass(fixedClass);
-    } else {
-      $navElement.removeClass(fixedClass);
-    }
-  };
-
-  // Toggle mobile navigation
-  $(".navigation__mobile-menu__toggle").click(function() {
-    $(this).parent().toggleClass('is-open');
-  });
+      // Toggle mobile navigation
+      $(".navigation__mobile-menu__toggle").click(function() {
+        $(this).parent().toggleClass('is-open');
+      });
 
       // Force close mobile navigation when clicking anywhere (except the toggle button itself)
-  $( document ).on('mousedown touchstart', function(event) {
-    if (!$(event.target).closest(".navigation__mobile-menu__toggle").length) {
-     // $(".navigation.is-open").removeClass('is-open');
-    }
-  });
-
-  $(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
-     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-       var target = $(this.hash);
-       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-       if (target.length) {
-         $('html,body').animate({
-           scrollTop: target.offset().top -58
-         }, 1000);
-         return false;
-       }
-     }
-    });
-   });
-
-  $(function() {
-
-    var selectorElement = '.navigation li a , .intro-section__left a',
-        targetElement = '.navigation__menu__item';
-
-    $(selectorElement).on('click touch', function(){
-      $(targetElement).removeClass('is-active');
-      $(this).parent().addClass('is-active');
-    });
-
-   });
+      $( document ).on('mousedown touchstart', function(event) {
+        if (!$(event.target).closest(".navigation__mobile-menu__toggle").length) {
+         // $(".navigation.is-open").removeClass('is-open');
+        }
+      });
 
   $(window).on('resize scroll', fixedHeader);
-
 });
